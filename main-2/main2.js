8def4edf292f966cd1798d29916529fd956e92d6
Guillermo Leon <guilevasa@gmail.com>
2024-07-04 09:54:36 -0500

    Se crea la carpeta main-2 donde se realizara ejercicio practico de clases

---
+++ main-2/main2.js
@@ -0,0 +1,11 @@
+class Student {
+    constructor ({
+        name,
+        email,
+        username,
+        twitter = undefined,
+        instagram = undefined,
+        facebook = undefined,
+        approvedCourses = [],
+    })
+}
