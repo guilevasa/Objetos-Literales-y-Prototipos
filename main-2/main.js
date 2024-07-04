e4ff6401eafc95ae0a20c6eacc64bf2a9ae2f97f
Guillermo Leon <guilevasa@gmail.com>
2024-07-04 10:54:37 -0500

    Se corrige el error poniendo ; al finalizar cada propiedad creada con this, y se crean las instancias para el objeto Student

--- main-2/main.js
+++ main-2/main.js
@@ -7,5 +7,32 @@ class Student {
         instagram = undefined,
         facebook = undefined,
         approvedCourses = [],
+        learningPaths = [],
     })
+    {
+        this.name = name;
+        this.email = email;
+        this.username = username;
+        this.socialMedia = {
+            twitter,
+            instagram,
+            facebook,
+        };
+        this.approvedCourses = approvedCourses;
+        this.learningPaths = learningPaths;
+    } // para las propiedades usadas con "this", finalizar con ";"
 }
+
+const juan2 = new Student ({
+    name: "JuanDC",
+    username: "juandc",
+    email: "juanito@juan.com",
+    instagram: "juanitodc",
+});
+
+const miguelito2 = new Student ({
+    name: "Miguel",
+    username: "miguelfeliz",
+    email: "migue@miguel.com",
+    facebook: "miguelacho",
+});
