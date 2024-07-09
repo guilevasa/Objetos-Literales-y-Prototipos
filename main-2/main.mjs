8ecd5f3e4a52ad13c9354821676d2c4b71588d02
Guillermo Leon <guilevasa@gmail.com>
2024-07-09 18:18:52 -0500

    Se crean dos funciones que haran que se reproduscan y pausen videos, se crea la clase PlatziClass donde se exportara solo la informacion dentro de esta clase, ocultando asi las urls de los videos a reproducir o pausar. Se crea un nuevo archivo module.mjs, se cambia la extension del main.js a main.mjs para poder manejar los modulos de JS, donde desde el archivo module.mjs se ejecutaran las clases que se hayan habilitado para ser exportadas, se importaran en el archivo module.mjs

---
+++ main-2/main.mjs
@@ -0,0 +1,154 @@
+function videoPlay (id) {
+    const urlSecreta = "https://urlvideosecreto.com " + id;
+    console.log("Se esta reporduciendo el video " + urlSecreta);
+}
+
+function videoPause (id) {
+    const urlSecreta = "https://urlvideosecreto.com " + id;
+    console.log("Video pausado " + urlSecreta);
+}
+
+// se registra "export" a la clase "PlatziClass" el cual va a referenciarla como la unica que podra ser utilizada desde el "index"
+export class PlatziClass {
+    constructor ({name, videoID}) {
+        this.name = name;
+        this.videoID = videoID;
+    }
+
+    reproducir() {
+        videoPlay(this.videoID);
+    }
+
+    pausar() {
+        videoPause(this.videoID);
+    }
+
+}
+
+class Course {
+    constructor ({name, classes = []}) {
+        this._name = name; // "_name" guion al piso para registrar que la propiedad sea mas privada al momento de realizarle alguna modificacion
+        this.classes = classes;
+    }
+
+    // sintaxis para utilizar "get & set" para usar las propiedades que se requieran ser mas privadas
+    get name() {
+        return this._name;
+    }
+
+    set name (nuevoNom) { // condiciones dadas para revisar la modificacion a la propiedad privada
+        if (nuevoNom === "Nombre inapropiado para el curso") {
+            console.error("Error");
+        } else {
+            this._name = nuevoNom;
+        }
+    }
+}
+
+const cursoProgBasica = new Course({
+    name: "Curso gratuito de programacion basica",
+});
+
+const cursoDefinitivoHTML = new Course({
+    name: "Curso definitivo de HTML y CSS",
+});
+
+const cursoPracticoHTML = new Course({
+    name: "Curso practico de HTML y CSS",
+});
+
+const cursoDataBus = new Course({
+    name: "Curso de data business",
+});
+
+const cursoDataViz = new Course({
+    name: "Curso de data Viz",
+});
+
+const cursoUnity = new Course({
+    name: "Curso de unity",
+});
+
+const cursoUnreal = new Course({
+    name: "Curso de unreal",
+});
+
+
+class LearningPath {
+    constructor ({name, courses = []}) {
+        this.name = name;
+        this.courses = courses;
+    }
+}
+
+const escuelaWeb = new LearningPath({
+    name: "Escuela de desarrollo web",
+    courses: [
+        cursoProgBasica,
+        cursoDefinitivoHTML,
+        cursoPracticoHTML,
+    ]
+});
+const escuelaData = new LearningPath({
+    name: "Escuela de data science",
+    courses: [
+        cursoProgBasica,
+        cursoDataBus,
+        cursoDataViz,
+    ]
+});
+const escuelaVgs = new LearningPath({
+    name: "Escuela de video games",
+    courses: [
+        cursoProgBasica,
+        cursoUnity,
+        cursoUnreal,
+    ]
+});
+
+class Student {
+    constructor ({
+        name,
+        email,
+        username,
+        twitter = undefined,
+        instagram = undefined,
+        facebook = undefined,
+        approvedCourses = [],
+        learningPaths = [],
+    })
+    {
+        this.name = name;
+        this.email = email;
+        this.username = username;
+        this.socialMedia = {
+            twitter,
+            instagram,
+            facebook,
+        };
+        this.approvedCourses = approvedCourses;
+        this.learningPaths = learningPaths;
+    } // para las propiedades usadas con "this", finalizar con ";"
+}
+
+const juan2 = new Student ({
+    name: "JuanDC",
+    username: "juandc",
+    email: "juanito@juan.com",
+    instagram: "juanitodc",
+    learningPaths: [
+        escuelaData,
+        escuelaVgs
+    ]
+});
+
+const miguelito2 = new Student ({
+    name: "Miguel",
+    username: "miguelfeliz",
+    email: "migue@miguel.com",
+    facebook: "miguelacho",
+    learningPaths: [
+        escuelaWeb,
+        escuelaVgs
+    ]
+});
---
+++ main-2/module.mjs
@@ -0,0 +1,15 @@
+import  {PlatziClass} from "./main.mjs";
+
+const clase67 = new PlatziClass ({
+    name: "JSOO basada en prototipos",
+    videoID: "Video #1"
+});
+
+clase67.reproducir();
+clase67.pausar();
+
+clase67.reproducir();
+clase67.pausar();
+
+clase67.reproducir();
+clase67.pausar();
